266ef43496bd7e06daa5b203cc13a3c952b72428
Andy Heggs <Andy@Heggs.net>
2024-09-11 10:34:54 +0100

    corrected 404 image link

--- middleware/agency-owner.js
+++ middleware/agency-owner.js
@@ -2,7 +2,7 @@
 const Agency = require('../models/agency'); 
 
 /**
- * Middleware to check if the user is the owner of the agency
+ * Middleware to check if user is owner of agency
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  * @param {Function} next - Express next middleware function
@@ -12,21 +12,21 @@ const isAgencyOwner = async (req, res, next) => {
     // Find agency by ID from request parameters
     const agency = await Agency.findById(req.params.agencyId);
     
-    // If agency is not found, respond with 404 status and render the 404 page
+    // If agency is not found, respond with 404 status and render 404 page
     if (!agency) {
       return res.status(404).render('404.ejs');
     }
 
-    // Check user in  session is owner agency
+    // Check user in session is owner agency
     if (agency.user.toString() !== req.session.user._id) {
       // If not, respond with 403 status and 'Access denied' message
       return res.status(403).send('Access denied');
     }
 
-    // If the user is owner, proceed to next middleware
+    // If user is owner, proceed to next middleware
     next();
   } catch (error) {
-    // Log any unexpected errors to the console
+    // Log any unexpected errors to console
     console.error(error);
     
     // Respond with 500 status and send 'Internal Server Error' message for any caught errors
